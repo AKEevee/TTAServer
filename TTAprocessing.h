67be748e38d9d791285932ff8c2d12f057b2c6f4
AKEevee <191370u@student.hci.edu.sg>
2021-08-17 19:54:28 +0800

    Commit

--- TTAprocessing.h
+++ TTAprocessing.h
@@ -2,25 +2,27 @@ using namespace std;
 namespace TTA
 {
 
-    class TTAP
-    {
-    public:
-        bool static timeContact(int time1, int time2) {
-            if(abs(time1-time2)<=259200) return true;
+    //class TTAP {
+    //public:
+        bool static timeContact(int64_t time1, int64_t time2) {
+            if (abs(time1 - time2) <= 259200) return true;
             else return false;
         }
-        bool static latContact(double lat1, double lat2){
-            if(abs(lat1-lat2)<=0.002) return true;
+
+        bool static latContact(double lat1, double lat2) {
+            if (abs(lat1 - lat2) <= 0.002) return true;
             else return false;
         }
-        bool static longtContact(double longt1, double longt2){
-            if(abs(longt1-longt2)<=0.002) return true;
+
+        bool static longtContact(double longt1, double longt2) {
+            if (abs(longt1 - longt2) <= 0.002) return true;
             else return false;
         }
-        bool static sameClient(const char *clientId1, const char *clientId2){
-            if(clientId1==clientId2) return true;
+
+        bool static sameClient(const char *clientId1, const char *clientId2) {
+            if (clientId1 == clientId2) return true;
             else return false;
         }
-    };
+    //};
 
 }
--- main.cpp
+++ main.cpp
@@ -14,7 +14,8 @@ main (int argc, char *argv[]) {
     bson_t *insert2;
     char *str2;
     //initialise variables
-    const char *uri_string = "mongodb://34.126.108.92:27017";
+    //const char *uri_string = "mongodb://34.126.108.92:27017";
+    const char *uri_string = "mongodb://localhost:27017";
     mongoc_uri_t *uri;
     mongoc_client_t *client;
     mongoc_database_t *database;
@@ -22,8 +23,8 @@ main (int argc, char *argv[]) {
     bson_t *command, reply, *insert;
     bson_error_t error;
     char *str;
-    mongoc_cursor_t *cursor;
-    const bson_t *doc;
+    mongoc_cursor_t *cursor, *cursor2;
+    const bson_t *doc, *doc2;
     bson_t *query, *opts;
 
     //Required to initialize libmongoc's internals
@@ -83,52 +84,109 @@ main (int argc, char *argv[]) {
     cout<<"Key in clientId of confirmed case: ";
     cin>>confirmedClient;
 
-        opts = BCON_NEW ("projection", "{",
-                         "time", BCON_BOOL (true),
-                         "lat", BCON_BOOL (true),
-                         "longt", BCON_BOOL (true),
-                         "clientId", BCON_BOOL (true),
-                         "}");
+        opts = BCON_NEW ("projection", "{","clientId", BCON_BOOL (true),"}");
 
         query = bson_new(); //empty query to query all data in collection
+        cursor = mongoc_collection_find_with_opts(collection1, query, opts, NULL);
+
+        //iterate thru data using cursor and find coordinates of confirmed case and store them in "cases" collection
+        while (mongoc_cursor_next(cursor, &doc)) {
+            //check if clientid is same as clientid of comfirmed case
+            bson_iter_t iter4, val2;
+            bson_iter_find_descendant(&iter4, "lat", &val2);
+            //insert to "cases" collection
+            if(confirmedClient == bson_iter_utf8(&val2,nullptr)) {
+                if (!mongoc_collection_insert_one(collection3, doc, NULL, NULL, &error)) {
+                    fprintf(stderr, "%s\n", error.message);
+                }
+            }
+            //delete document from coordinates collection (for now not yet)
+            /*if (!mongoc_collection_remove (collection1, MONGOC_REMOVE_SINGLE_REMOVE, doc, NULL, &error)) {
+                printf ("Delete failed: %s\n", error.message);
+            }*/
+    }
+        opts = BCON_NEW ("projection", "{",
+                     "time", BCON_BOOL (true),
+                     "lat", BCON_BOOL (true),
+                     "longt", BCON_BOOL (true),
+                     "clientId", BCON_BOOL (true),
+                     "}");
         cursor = mongoc_collection_find_with_opts(collection1, query, opts, NULL);
         while (mongoc_cursor_next(cursor, &doc)) {
             //str2 = bson_as_canonical_extended_json(doc, NULL);
             //print bson data that has been read
             //printf("%s\n", str2);
 
-            //insert to "matches" collection
-            /*if (!mongoc_collection_insert_one(collection2, doc, NULL, NULL, &error)) {
-                fprintf(stderr, "%s\n", error.message);
-            }*/
 
-            bson_iter_t iter2, val;
-            int64_t x;
-            const char *str3;
+            bson_iter_t iter2, iter5, val, val2;
+            int64_t t1, t2;
+            double la1, la2, lo1, lo2;
+            const char *id1, *id2;
 
             //iterate thru data (and store them to c variables in the future)
 
             if ( bson_iter_init(&iter2, doc) &&
                 bson_iter_find_descendant(&iter2, "clientId", &val) ) {
-                str3 = bson_iter_utf8(&val,nullptr);
-                cout<< "clientId = " << str3 << endl;
+                id1 = bson_iter_utf8(&val,nullptr);
+                cout<< "clientId = " << id1 << endl;
+            }
+
+            if( sameClient(id1,confirmedClient) ) continue;
+
+            if ( bson_iter_init(&iter2, doc) &&
+                bson_iter_find_descendant(&iter2, "lat", &val) ) {
+                cout.precision(10);
+                la1 = bson_iter_as_double(&val);
+                cout<< "lat = " << la1 << endl;
             }
 
             if ( bson_iter_init(&iter2, doc) &&
                 bson_iter_find_descendant(&iter2, "lat", &val) ) {
                 cout.precision(10);
-                cout<< "lat = " << bson_iter_as_double(&val) << endl;
-            }
-
-            if ( bson_iter_init(&iter2, doc) &&
-                 bson_iter_find_descendant(&iter2, "lat", &val) ) {
-                cout.precision(10);
-                cout<< "longt = " << bson_iter_as_double(&val) << endl;
+                lo1 = bson_iter_as_double(&val);
+                cout<< "longt = " << lo1 << endl;
             }
 
             if ( bson_iter_init(&iter2, doc) &&
                 bson_iter_find_descendant(&iter2, "time", &val) ) {
-                cout<< "time = " << bson_iter_as_int64(&val) << endl;
+                t1 = bson_iter_as_int64(&val);
+                cout<< "time = " << t1 << endl;
+            }
+            cursor2 = mongoc_collection_find_with_opts(collection3, query, NULL, NULL);
+            while (mongoc_cursor_next(cursor2, &doc2)) { //confirmed cases
+
+                if ( bson_iter_init(&iter5, doc2) &&
+                     bson_iter_find_descendant(&iter5, "clientId", &val2) ) {
+                    id2 = bson_iter_utf8(&val2,nullptr);
+                }
+
+                if( sameClient(id1,id2) ) continue;
+
+                if ( bson_iter_init(&iter5, doc2) &&
+                     bson_iter_find_descendant(&iter5, "lat", &val2) ) {
+                    la2 = bson_iter_as_double(&val2);
+                }
+
+                if( !latContact(la1,la2) ) continue;
+
+                if ( bson_iter_init(&iter5, doc2) &&
+                     bson_iter_find_descendant(&iter5, "lat", &val2) ) {
+                    lo2 = bson_iter_as_double(&val2);
+                }
+
+                if( !longtContact(lo1,lo2) ) continue;
+
+                if ( bson_iter_init(&iter5, doc2) &&
+                     bson_iter_find_descendant(&iter5, "time", &val2) ) {
+                    t2 = bson_iter_as_int64(&val2);
+                }
+                if ( timeContact(t1,t2) ) {
+                    cout<<"Match Found"<<endl;
+                    //insert to "matches" collection
+                    if (!mongoc_collection_insert_one(collection2, doc, NULL, NULL, &error)) {
+                        fprintf(stderr, "%s\n", error.message);
+                    }
+                }
             }
 
             bson_free(str2);
